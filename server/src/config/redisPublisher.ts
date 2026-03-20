7949d26cd268a20711e2d1e9b5fe57164b73f73b
PrithwisK07 <karmakarprithwis566@gmail.com>
2026-03-20 20:16:22 +0530

    deploy changes

--- server/src/config/redisPublisher.ts
+++ server/src/config/redisPublisher.ts
@@ -1,6 +1,11 @@
 import IORedis from 'ioredis';
+import dotenv from 'dotenv';
 
-export const redisPublisher = new IORedis({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: parseInt(process.env.REDIS_PORT || '6379'),
-});
+dotenv.config();
+
+export const redisPublisher = process.env.REDIS_URL 
+  ? new IORedis(process.env.REDIS_URL)
+  : new IORedis({
+      host: process.env.REDIS_HOST || 'localhost',
+      port: parseInt(process.env.REDIS_PORT || '6379'),
+    });
